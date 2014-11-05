48911c19de6f7f2d1c5d77cd205dea66c15bd3f0
Mingchen Shen <mshen@payjunction.com>
2014-11-05 10:53:57 -0800

    added remove/checking functionality

--- shoppermate.js
+++ shoppermate.js
@@ -1,11 +1,37 @@
+function removeListItem(){
+	$('.btn-remove').on('click', function(){
+		$(this).closest('.listitem').remove();
+	});
+};
+
+function toggleCheck(){
+	$('.listitem').on('click', function(){
+		$(this).toggleClass('checked');
+		$(this).find('.btn-check').toggleClass('fa fa-check');
+	});
+
+};
+
 $(document).ready(function(){
+	//setting functionality
+	removeListItem();
+	toggleCheck();
+
+
 	$(".listinput").on('keypress', function(){
 		if(event.which == 13){
-		event.preventDefault();
-		var item = $(this).val();
-		// console.log(item);
-		$(".list").append("<div class='listitem'>" + item + "</div>");
-		$(this).val('');
+			event.preventDefault();
+			var item = $(this).val();
+			if(jQuery.trim(item).length == 0){
+				console.log('please enter text');
+				$(this).val('');
+			}else{
+				$(".list").prepend("<div class='listitem'>" + item + "<div class='btn-remove fa fa-times'></div></div>");
+				$(this).val('');
+				//setting functionality
+				removeListItem();
+				toggleCheck();
+			};
 		};
 	});
-})
+});
